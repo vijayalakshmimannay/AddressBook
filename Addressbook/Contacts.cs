d2c4efb1fc101c94fc951b7ba83fbc6f49cac545
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-10 23:32:53 +0530

    [vijayalakshmi] create contacts

---
+++ Addressbook/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook
+{
+    internal class Contacts
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public int phoneNo { get; set; }
+        public string email { get; set; }
+    }
+}
